2b4d9f8f8a4c7f2a5e26063c98117599b81ce09e
kokada60 <kokada60@gmail.com>
2021-11-10 16:12:46 -0800

    Distance is done Also Purrr2

--- Purrr2.R
+++ Purrr2.R
@@ -9,7 +9,7 @@ list(mtcars, mtcars %>% nest_by(key=cyl)) %>%
   reduce(inner_join, by=c("cyl" = "key"))
 
 library(stringr)
-install.packages("tidygraph", "ggraph")
+install.packages(c("tidygraph", "ggraph"))
 library(tidygraph)
 library(ggraph)
 
@@ -47,10 +47,69 @@ map_chr(dat, d_or_a)
 
 #Find character with empty "alias" list...
 map(dat, "aliases") %>% map_dbl( ~ length(.)) %>% { . == 0 } %>% which()
+dat[map(dat, "name") %>% {. == "Jon Snow"} %>% which()]
 
-  
-  {. -1} %>% 
-  {. == 0 } %>% which(.)
+
+dat[[18]]$aliases
+
+g <- dat
+g <- map(dat, compact)
+
+also_known_as <- function(x) {
+  if("aliases" %in% names(x)) {
+    g <- tibble(
+      from = x$name,
+      to = x$aliases
+    )
+    g <- as_tbl_graph(g) 
+  }
+}
+
+g <- purrr::map(g, also_known_as) 
+g
+
+ggraph(g[[23]], layout="graphopt") + 
+  geom_edge_link() + 
+  geom_node_label(aes(label= name),
+                  label.padding=unit(1, "lines"), 
+                  label.size=0) +
+  theme_graph()
+
+ggraph(g[[23]], layout = "graphopt") + 
+  geom_edge_link() + 
+  geom_node_label(aes(label = name), 
+                  label.padding = unit(1, "lines"), 
+                  label.size = 0) +
+  theme_graph()
+
+
+intersect(dat_m$allegiances[16] %>% unlist(), c("House Stark of Winterfell")) %>% length()> 0
+
+dat_m$allegiances[16:18]
+dat_m$allegiances[16]
+dat_m$allegiances[17]
+dat_m$allegiances[18]
+
+dat_p <- dat_m %>% mutate(stark_or_lanister = map(allegiances, ~ str_extract(.x, "Lannister|Stark") %>% 
+                                                    discard(is.na))) # str_extract "extracts" the specified segments from the string. 
+dat_p %>% unnest(stark_or_lanister) # revealing the filtered allegiance list. Possible that a house could have allegiance to both Stark and Lanister.
+dat_p %>% mutate(Cnt_Of_houses_Allged_to = map_int(stark_or_lanister, ~ length(.))) %>% 
+  filter(Cnt_Of_houses_Allged_to > 0)
+
+dat_p %>% mutate(ssa = map(allegiances, paste, collapse="#")  %>% unlist())
+
+
+
+
+
+
+
+dat_p$stark_or_lanister[[2]] %>% length()
+
+
+
+
+
 
 
 
